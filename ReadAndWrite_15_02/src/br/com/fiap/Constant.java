011e7ed0e0f897219935bbd285b60fd4a90fb651
Vitor Hugo <vitorh565.vh@gmail.com>
2024-02-15 17:51:44 -0300

    15_02

--- ReadAndWrite_15_02/src/br/com/fiap/Constant.java
+++ ReadAndWrite_15_02/src/br/com/fiap/Constant.java
@@ -3,8 +3,10 @@ package br.com.fiap;
 public class Constant {
     public static void main(String[] args) {
         // Constants in Java
-        // The value can't be changed
+        // The value can't be changed during all program
+        // ALL WORDS IN UPPER
 
-        final double pi = 3.14;
+        final double PI = 3.1416;
+
     }
 }
--- ReadAndWrite_15_02/src/br/com/fiap/Primitives.java
+++ ReadAndWrite_15_02/src/br/com/fiap/Primitives.java
@@ -2,6 +2,22 @@ package br.com.fiap;
 
 public class Primitives {
     public static void main(String[] args) {
-        
+        char gender = 'M'; //Here is necessary to use a simple '', and the type char only stock a single value
+
+        byte age = 19; // Can be use to stock ages, little numbers (-127 to 127)
+
+        short code = 32000; // ask to the teacher, why.
+
+        float average = 4.5f; // ask also.
+
+        int students = 100, classes = 4;
+
+        long brazilians = 216535748;
+
+        double dolar = 5.15;
+
+        boolean alternative = true;
+
+        System.out.println("Average: "+ average + " Students: "+ students + " Classes: " + classes);
     }
 }
