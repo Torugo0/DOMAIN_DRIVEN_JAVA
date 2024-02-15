627b610380925e7fcfb9b626a8f41210b02a5807
Vitor Hugo <vitorh565.vh@gmail.com>
2024-02-15 09:46:08 -0300

    Necessary do the examples

---
+++ ReadAndWrite_15_02/src/br/com/fiap/Constant.java
@@ -0,0 +1,10 @@
+package br.com.fiap;
+
+public class Constant {
+    public static void main(String[] args) {
+        // Constants in Java
+        // The value can't be changed
+
+        final double pi = 3.14;
+    }
+}
--- ReadAndWrite_15_02/src/br/com/fiap/Primitives.java
+++ ReadAndWrite_15_02/src/br/com/fiap/Primitives.java
@@ -1,5 +1,7 @@
 package br.com.fiap;
 
 public class Primitives {
-
+    public static void main(String[] args) {
+        
+    }
 }
