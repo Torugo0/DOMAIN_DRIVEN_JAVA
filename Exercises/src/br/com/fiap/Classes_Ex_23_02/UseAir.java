2690226bc0cb119611525a561d68b29a76d93af7
Vitor Hugo <vitorh565.vh@gmail.com>
2024-02-26 13:20:39 -0300

    Finish

--- Exercises/src/br/com/fiap/Classes_Ex_23_02/UseAir.java
+++ Exercises/src/br/com/fiap/Classes_Ex_23_02/UseAir.java
@@ -4,6 +4,14 @@ public class UseAir {
     public static void main(String[] args) {
         AirConditioning air = new AirConditioning();
 
-        
+        air.mode = "Cool";
+        air.temperature = 18;
+        air.show();
+
+        air.lowerTemperature();
+        air.show();
+
+        air.changeMode("Dry");
+        air.show();
     }
 }
